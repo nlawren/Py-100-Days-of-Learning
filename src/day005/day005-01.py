692cdfae36821057ebc3cb8fd14af9ac8464160f
Nicholas Lawrence <nlawren@gmail.com>
2023-06-28 12:18:16 +1000

    Renaming files and start day5 e2

--- src/day005/day005-01.py
+++ src/day005/day005-01.py
@@ -1,5 +1,19 @@
-fruits = ["Apple", "Peach", "Pear"]
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
 
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " Pie")
+
+# Write your code below this row 👇
+
+total_height = 0
+num_students = 0
+
+for height in student_heights:
+    total_height += height
+    num_students += 1
+
+average_height = round(total_height / num_students)
+
+print(average_height)
