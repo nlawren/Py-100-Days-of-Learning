04092ff3b3d234e00877d29d8ef7ff6ae245d2fd
Nicholas Lawrence <nlawren@gmail.com>
2024-05-16 17:26:12 +1000

    100 days of python - completed day7 task 4

--- src/day007/day007-04.py
+++ src/day007/day007-04.py
@@ -110,6 +110,9 @@ while not end_of_game:
     # Then reduce 'lives' by 1.
     # If lives goes down to 0 then the game should stop and it should print "You lose."
 
+    # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number
+    # of 'lives' the user has remaining.
+    print(stages[lives])
     # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
@@ -117,5 +120,3 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
-
-    # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
