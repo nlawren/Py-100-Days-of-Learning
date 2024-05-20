e941826604dfdb0b85ef034d2de1a9082337d287
Nicholas Lawrence <nlawren@gmail.com>
2024-05-20 19:41:03 +1000

    100 Days of Python Day 8 - completing the prime number checker.

--- src/day008/day08-task3.py
+++ src/day008/day08-task3.py
@@ -1,15 +1,13 @@
 # Write your code below this line 👇
 def prime_checker(number):
-    if number > 1:
-        for i in range(2, (number // 2) + 1):
-            if (number % i) == 0:
-                print("It's not a prime number.")
-                break
-            else:
-                print("It's a prime number.")
-                break
-        else:
-            print("It's not a prime number.")
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
 
 
 # Write your code above this line 👆
