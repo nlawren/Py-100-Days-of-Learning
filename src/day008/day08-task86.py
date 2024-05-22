74223877ed6247070bc2d29fdcf0681f9bd66d79
Nicholas Lawrence <nlawren@gmail.com>
2024-05-22 19:23:58 +1000

    100 Days of Python Day 8 - Ceasars Cipher - Encode Adding a summary print

--- src/day008/day08-task86.py
+++ src/day008/day08-task86.py
@@ -49,6 +49,7 @@ def encrypt(text, shift):
     print(f"The encoded text is {''.join(cipher_text)}")
 
 
+print("Day 8 - Ceasars Cipher - Task 86\n")
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
