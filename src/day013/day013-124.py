5c799f4053c754a88f875eef5074314d4c0acaff
Nicholas Lawrence <nlawren@gmail.com>
2024-06-21 19:21:52 +1000

    100 Days of Python Day 13 - Debugging Working through

--- src/day013/day013-124.py
+++ src/day013/day013-124.py
@@ -4,11 +4,13 @@
 ############DEBUGGING#####################
 
 # # Describe Problem
-# def my_function():
-#   for i in range(1, 20):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
 
 # # Reproduce the Bug
 # from random import randint
