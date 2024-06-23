a9708a8d5611830bff280f03a91af8ce6c887be6
Nicholas Lawrence <nlawren@gmail.com>
2024-06-23 17:39:04 +1000

    100 Days of Python Day 13 - Debugging More debugging

--- src/day013/day013-124.py
+++ src/day013/day013-124.py
@@ -34,9 +34,9 @@ if age > 18:
 pages = 0
 word_per_page = 0
 pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(total_words)
+word_per_page == int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
 
 # #Use a Debugger
 # def mutate(a_list):
