1a80bea2dfebd85b9a479492edaa0d1888006fc4
Nicholas Lawrence <nlawren@gmail.com>
2024-11-16 19:29:03 +1100

    Added ruff to the project Completed Coding exercise 9 (day 9 task 93.

--- src/day009/day009-task93.py
+++ src/day009/day009-task93.py
@@ -1,18 +1,30 @@
 # Day 9 Dictionaries
+# Coding Exercise 9
 
 student_scores = {
-    "Harry": 81,
+    "Harry": 88,
     "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62,
+    "Hermione": 95,
+    "Draco": 75,
+    "Neville": 60,
 }
 # 🚨 Don't change the code above 👆
 # TODO-1: Create an empty dictionary called student_grades.
 student_grades = {}
 
 # TODO-2: Write your code below to add the grades to student_grades.👇
-
+for name in student_scores:
+    score = student_scores[name]
+    print(f"Assessing {name} with a score of {score}")
+    if score > 90 and score <= 100:
+        grade = "Outstanding"
+    elif score > 80 and score <= 90:
+        grade = "Exceeds Expectations"
+    elif score > 70 and score <= 80:
+        grade = "Acceptable"
+    else:
+        grade = "Fail"
+    student_grades[name] = grade
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
