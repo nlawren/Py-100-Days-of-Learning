268dbe87e7ae070c9c977b1d08f49148e599bb94
Nicholas Lawrence <nlawren@gmail.com>
2024-12-16 18:56:17 +1100

    Day 10 exercises

---
+++ src/day010/day010-task073.py
@@ -0,0 +1,12 @@
+# Day 10 Task 73 Functions with outputs
+
+
+def format_name(f_name, l_name):
+    format_f_name = f_name.title()
+    format_l_name = l_name.title()
+
+    print(f"{format_f_name} {format_l_name}")
+    return "format_f_name format_l_name"
+
+
+format_name(f_name="angela", l_name="YU")
