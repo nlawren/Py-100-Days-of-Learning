3c835011b829f87ed87e29d643df9ead7e3bbd90
Nicholas Lawrence <nlawren@gmail.com>
2025-04-13 14:28:20 +1000

    Day 10 coding exercise 10

--- src/day010/day010-coding exercise 10.py
+++ src/day010/day010-coding exercise 10.py
@@ -11,13 +11,11 @@ def is_leap(year):
         print("Not leap year")
 
 
-# TODO: Add more code here 👇
 def days_in_month():
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     print(month_days)
 
 
-# 🚨 Do NOT change any of the code below
 year = int(input())  # Enter a year
 month = int(input())  # Enter a month
 days = days_in_month(year, month)
