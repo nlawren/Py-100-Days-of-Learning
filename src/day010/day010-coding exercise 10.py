b93be5088a70958fbf395c7683c76e2e4e023812
Nicholas Lawrence <nlawren@gmail.com>
2025-04-13 18:03:48 +1000

    Completed coding exercise 10

--- src/day010/day010-coding exercise 10.py
+++ src/day010/day010-coding exercise 10.py
@@ -1,22 +1,18 @@
-def is_leap(year):
+def is_leap_year(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                print("Leap year")
+                # Leap year
+                return True
             else:
-                print("Not leap year")
+                # Not a leap year
+                return False
         else:
-            print("Leap year")
+            # Leap year
+            return True
     else:
-        print("Not leap year")
+        # Not a leap year
+        return False
 
 
-def days_in_month():
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    print(month_days)
-
-
-year = int(input())  # Enter a year
-month = int(input())  # Enter a month
-days = days_in_month(year, month)
-print(days)
+is_leap_year(2024)
